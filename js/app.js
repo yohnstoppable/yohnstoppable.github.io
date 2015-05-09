1af0bd9b1f019d4c41d77eb33c282743c8ab0d82
yohnstoppable <yohnster@gmail.com>
2015-05-09 00:27:00 -0500

    now kith
    beginning angular shtuff

---
+++ js/app.js
@@ -0,0 +1,10 @@
+(function(){
+	var app = angular.module('newNative', []);
+		
+	app.controller("menuController",function() {
+		this.menuItems = ["HOME","TIES","COATS","SHOES","BIO"];
+	});
+
+	//app.directive("menu",
+	var menuItems = ["HOME","TIES","COATS","SHOES","BIO"];		
+})();
