9f0dfbd155184e87265a0d27e147782b0d2a5ecd
yohnstoppable <yohnster@gmail.com>
2015-05-12 16:09:56 -0500

    oh, hi mark

--- js/app.js
+++ js/app.js
@@ -2,40 +2,43 @@
 	var app = angular.module('newNative', []);
 		
 	app.controller("menuController",function() {
-		this.menuItems = ["HOME","TIES","COATS","SHOES","BIO"];
-		this.images = images;
-		this.section = "coats";
-		
-		this.clicked = function(menuItem) {
-			this.section = menuItem;
-		}
-		
-		this.sectionType = function(image) {
-			return image.substring(0, str.length - 1);
-		}
+		this.menuItems = [];
+		this.menuItems.push("Home");
+		for (var i=0; i < results.length; i++) {
+			if (this.menuItems.indexOf(results[i].category_path[0]) === -1) {
+				this.menuItems.push(results[i].category_path[0]);
+			}
+		}	
+		this.menuItems.push("Bio");
 	});
 	
 	app.controller("imageController", function() {
 		this.items = items;
 	});
+	
+	app.controller("master", function($http){		
+		this.results = results;
+		console.log(results);
+		
+		this.section = "Furniture";
+		
+		this.clicked = function(menuItem) {
+			this.section = menuItem;
+		}
+		
+		this.details = function(header,img,description,url) {
+			swal({
+				title: header,
+				text: "<a href=" + url + "><img class='modalImage' src='" + img + "'></a><br><br>" + description.replace(/(?:\r\n|\r|\n)/g, '<br />'),
+				html: true,
+				backgroundColor: "#cccc99",
+				confirmButtonColor: "#70909E",
+				allowOutsideClick:"true"
+			});
+		}
+	});
 
 	var menuItems = ["HOME","TIES","COATS","SHOES","BIO"];	
-	var images = ["tie1","tie2","tie3","tie4","tie5","tie6","coat1","coat2","coat3","shoes1","shoes2","shoes3"];
-	var items = [{
-		title: "Tie 1",
-		section: "ties",
-		description: "Robert Downey Jr is one sexy mother fucker. I always forget that he was the guy in Weird Science who pantsed the two main guys at the start. That was a dick move. Also, Kiss Kiss Bang Bang is an awesome movie",
-		imageUrl: "images/tie1.jpg"
-	}, {
-		title: "Tie 2",
-		section: "ties",
-		description: "Gerard Butler is a little nancy boy who has to ask permission to use the restroom. He tried to fight me over using the blue play doh, but I whooped his ass",
-		imageUrl: "images/tie2.jpg"
-	}, {
-		title: "Tie 3",
-		section: "ties",
-		description: "I don't even know who this mother fucker is. I bet he has sex with his socks on like a goober",
-		imageUrl: "images/tie3.jpg"
-	}
-	]
+	var sections = [];
+	var section = "ties";			
 })();
